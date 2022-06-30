eb23c3ae9eb4e821483c9a0c0ba3b68dfb1a8ee0
Evgeniy <e.vasiliev.86@mail.ru>
2022-06-30 05:13:35 +0300

    функция for

---
+++ Example009/Program.cs
@@ -0,0 +1,37 @@
+﻿// Заменить в тексте все пробелы на черточки. Маленькие буквы к на большие К, а большие С на маленькие С.
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+             + "ежели бы вас послали вместо нашего милого"
+             + "Винценгероде, вы бы взяли приступом согласие"
+             + "прусского короля. Вы так красноречивы. Вы"
+             + "дадите мне чаю?";
+
+//string s = "qwerty"
+//            012345
+//s[3]  //  r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+System.Console.WriteLine(newText);
+System.Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+System.Console.WriteLine(newText);
+System.Console.WriteLine();
+newText = Replace(newText, 'С', 'с');
+System.Console.WriteLine(newText);
+
+
+
